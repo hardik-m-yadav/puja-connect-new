b2ef3ae0651516670c3ff942004f825313c66ca8
hardik-m-yadav <106054777+hardik-m-yadav@users.noreply.github.com>
2026-07-07 15:15:13 +0530

    Fix import case for CTA component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import RitualShowcase from "../components/sections/RitualShowcase";
  import HowItWorks from "../components/sections/HowItWorks.jsx";
  import Testimonials from "../components/sections/Testimonials.jsx";
  import WhyChooseUs from "../components/sections/WhyChooseUs.jsx";
-   import CTA from "../components/sections/cta"
+   import CTA from "../components/sections/CTA"
  import FAQ from "../components/sections/FAQ.jsx";
 import AIGuruCTA from "../components/sections/AICTA.jsx";
 
